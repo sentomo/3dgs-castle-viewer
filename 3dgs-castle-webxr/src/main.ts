fec096240845c54e32f528e68e3f000898612ee3
sentomo <tomoko.senju@jrits.ricoh.co.jp>
2025-03-06 03:14:14 +0900

    Delete sphere

--- 3dgs-castle-webxr/src/main.ts
+++ 3dgs-castle-webxr/src/main.ts
@@ -59,16 +59,6 @@ const main = async () => {
   // Add a camera for the non-VR view in browser
   const camera = new ArcRotateCamera("Camera", -(Math.PI / 4) * 3, Math.PI / 4, 10, new Vector3(0, 0, 0), scene);
   camera.attachControl(true);
-   
-  // Add a sphere to have something to look at
-  const sphereD = 1.0;
-  const sphere = MeshBuilder.CreateSphere("xSphere", { segments: 16, diameter: sphereD }, scene);
-  sphere.position.x = 0;
-  sphere.position.y = sphereD * 2;
-  sphere.position.z = 0;
-  const rMat = new StandardMaterial("matR", scene);
-  rMat.diffuseColor = new Color3(1.0, 0, 0);
-  sphere.material = rMat;
 
   // Clock
   const clock = createClock(scene);
