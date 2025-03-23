549d9dfdc7786db14ba4e3bd569dd5cf7c7b6690
sentomo <tomo1010.0594@gmail.com>
2025-03-23 17:54:31 +0000

    fix: Re-adjusted spz display position, removed floor for VR

--- 3dgs-castle-webxr/src/main.ts
+++ 3dgs-castle-webxr/src/main.ts
@@ -50,8 +50,7 @@ const main = async () => {
   const envHelper = new EnvironmentHelper(
     {
       skyboxSize: 200,
-      groundSize: 100,
-      groundColor: new Color3(0.5, 0.5, 0.5),
+      createGround: false,
     },
     scene,
   );
@@ -65,7 +64,7 @@ const main = async () => {
   
   // 3dgs
   const gaussianSplatting = await loadAssetContainerAsync("https://raw.githubusercontent.com/sentomo/3dgs-castle-viewer/master/3dgs-castle-webxr/src/assets/KakegawaCastle.spz", scene);
-  gaussianSplatting.meshes[0].position = new Vector3(40.0, 38.5, 30.0);
+  gaussianSplatting.meshes[0].position = new Vector3(40.0, 38.0, 30.0);
   gaussianSplatting.meshes[0].rotation.y = 45.0;
   gaussianSplatting.meshes[0].scaling = new Vector3(2.0, 2.0, 2.0);
 
